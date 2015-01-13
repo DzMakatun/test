78e91a85020bc907060b43f33c83548e6e0e00a9
DzMakatun <d.i.makatun@gmail.com>
2015-01-13 12:58:33 +0100

    changed output message

--- de.vogella.git.first/src/de/vogella/git/first/TestGit.java
+++ de.vogella.git.first/src/de/vogella/git/first/TestGit.java
@@ -2,7 +2,7 @@ package de.vogella.git.first;
 
 public class TestGit {
 	public static void main(String[] args) {
-	    System.out.println("Git is fun");
+	    System.out.println("Git is cool");
 	  }
 	
 }
